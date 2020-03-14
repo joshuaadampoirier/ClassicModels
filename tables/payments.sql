42e07c87cbf73b30bfa434e3565826615b285aff
Joshua Poirier <joshuapoirier@Joshuas-Air.fios-router.home>
2020-03-14 14:57:39 -0400

    payments table definition

---
+++ tables/payments.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS payments 
+(
+    /*  column definitions */
+    customer_number     INTEGER         NOT NULL 
+    ,check_number       VARCHAR(50)     NOT NULL 
+    ,payment_date       DATE            NOT NULL 
+    ,amount             NUMERIC(10, 2)  NOT NULL 
+
+    /*  primary key definition */
+    ,PRIMARY KEY    (customer_number, check_number)
+
+    /*  foreign key definitions */
+    ,FOREIGN KEY    customer_number REFERENCES customers (customer_number)
+);
