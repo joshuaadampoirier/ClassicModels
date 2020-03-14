205ce7f9df8b3993c129884ad92af9906cd9383c
Joshua Poirier <joshuapoirier@Joshuas-Air.fios-router.home>
2020-03-14 14:57:17 -0400

    offices table definition

---
+++ tables/offices.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS offices 
+(
+    /*  column definitions */
+    office_code         VARCHAR(10)         NOT NULL 
+    ,city               VARCHAR(50)         NOT NULL 
+    ,phone              VARCHAR(50)         NOT NULL 
+    ,address_line_1     VARCHAR(50)         NOT NULL 
+    ,address_line_2     VARCHAR(50)         DEFAULT NULL 
+    ,state              VARCHAR(50)         DEFAULT NULL 
+    ,country            VARCHAR(50)         NOT NULL 
+    ,postal_code        VARCHAR(15)         NOT NULL 
+    ,territory          VARCHAR(10)         NOT NULL 
+
+    /*  primary key definition */
+    ,PRIMARY KEY    office_code 
+);
