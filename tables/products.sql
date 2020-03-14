4c1755f4e70f8ce56c03a7e4b572518714d489b3
Joshua Poirier <joshuapoirier@Joshuas-Air.fios-router.home>
2020-03-14 14:58:42 -0400

    products table definition

---
+++ tables/products.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS products 
+(
+    /*  column definitions */
+    product_code            VARCHAR(15)     NOT NULL 
+    ,product_name           VARCHAR(70)     NOT NULL 
+    ,product_line           VARCHAR(50)     NOT NULL 
+    ,product_scale          VARCHAR(10)     NOT NULL 
+    ,product_vendor         VARCHAR(50)     NOT NULL 
+    ,product_description    TEXT            NOT NULL 
+    ,quantity_in_stock      SMALLINT        NOT NULL 
+    ,buy_price              NUMERIC(10, 2)  NOT NULL 
+    ,msrp                   NUMERIC(10, 2)  NOT NULL 
+
+    /*  primary key definition */
+    ,PRIMARY KEY    product_code 
+
+    /*  foreign key definitions */
+    ,FOREIGN KEY    product_line REFERENCES product_lines (product_line)
+);
